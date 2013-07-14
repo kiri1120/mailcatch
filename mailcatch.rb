61b939346fb5acd3d8a06aec26691f5d0628ae9f
kiri1120 <kazuki.s1120@gmail.com>
2013-07-14 20:57:00 +0900

    完成？

--- mailcatch.rb
+++ mailcatch.rb
@@ -7,25 +7,20 @@ require 'json'
 require 'base64'
 require 'httpclient'
 
-config_path = File.expand_path('../config/config.json', __FILE__)
-config = JSON.parse(File.read(config_path), {:symbolize_names => true})
-
 email = Mail.new(STDIN.read)
 
-from    = email.from.first
-to      = email.to.first
-subject = email.subject
-
 if  email.multipart?
-  body = email.text_part.body
+  body_part = email.text_part.body
 else
-  body = email.body
+  body_part = email.body
 end
 
+product_name, domain = email.to.first.split('@')
+
 data = {
-  from:         from,
-  subject:      subject,
-  body:         NKF.nkf('-w', body.to_s.strip),
+  from:         email.from.first,
+  subject:      email.subject,
+  body:         NKF.nkf('-w', body_part.to_s.strip),
   attachments:  []
 }
 
@@ -42,37 +37,33 @@ if email.has_attachments?
   end
 end
 
-json_data = JSON.generate(data)
-product_name, domain = to.split('@')
-error_from = 'MAILER-DAEMON@' + domain
-uri = "http://localhost/#{product_name}/mailcatch/"
-
-client = HTTPClient.new
-
 begin
-  client.post_content(uri, json_data, 'Content-Type' => 'application/json')
+  client = HTTPClient.new
+  client.post_content(
+    "http://localhost/#{product_name}/mailcatch/",
+    JSON.generate(data),
+    'Content-Type' => 'application/json'
+  )
 rescue
-  # error mail
-  Mail.defaults do
-    delivery_method :smtp, config[:smtp_setting]
-  end
+  error_body = "This is the mail system at host #{domain}.
 
-  Mail.deliver({
-    to:       from,
-    from:     error_from,
-    subject:  'Undelivered Mail Returned to Sender',
-    body:     "This is the mail system at host #{domain}.
-
-I'm sorry to have to inform you that your message could not\nbe delivered to one or more recipients. It's attached below.
+I'm sorry to have to inform you that your message could not be delivered to one or more recipients. It's attached below.
 
 For further assistance, please send mail to postmaster.
 
-If you do so, please include this problem report. You can
-delete your own text from the attached returned message.
+If you do so, please include this problem report. You can delete your own text from the attached returned message.
 
                    The mail system
 
-<#{to}>: unknown user: \"#{product_name}\""
-  })
+<#{product_name}@#{domain}>: unknown user: \"#{product_name}\""
+
+  # error mail
+  mail = Mail.deliver do
+    delivery_method(:sendmail)
+    to(data[:from])
+    from('postmaster@' + domain)
+    subject('Undelivered Mail Returned to Sender')
+    body(error_body)
+  end
 end
 
